430745d84c35935808e53f47eb38e6143d37d7ee
AjmalOP <ajmalop22@gmail.com>
2023-12-18 11:21:54 +0530

    spell update

--- src/assets/Home.js
+++ src/assets/Home.js
@@ -5,8 +5,8 @@ let Home = ()=>{
     const NavigateToCreate = useNavigate()
     return (
         <div style={{marginTop:'250px'}}>
-        <h3 style={{color:'white'}}>Here We Create and Share Your Blog...</h3>
-        <h1 style={{color:'yellow'}}><span style={{color:'red'}}>Create</span><i> Your On Blog </i><span style={{color:'green'}}>With Us</span></h1>
+        <h3 style={{color:'white'}}>Here You Can Create and Share Your Blog...</h3>
+        <h1 style={{color:'yellow'}}><span style={{color:'red'}}>Create</span><i> Your Own Blog </i><span style={{color:'green'}}>With Us</span></h1>
             <button onClick={()=>NavigateToCreate('/create')} style={{backgroundColor:'black',border:'1px solid rgb(100, 40, 40)', padding:'8px' , borderRadius:'8px' , color:'white' , fontSize:'15px'}}><b>Create New Blog</b></button>
         </div>
     )
